e286bdb0eb3c4d1bca81ac001fa904cf13b7eff8
Jean-Pierre Coffe <houzet_alex@yahoo.fr>
2017-11-10 17:05:14 +0100

    j'ai rajouté la page premium

--- premium.php
+++ premium.php
@@ -9,6 +9,15 @@ require_once 'src/app.php';
 require_once 'view/header.php';
 ?>
 
+
+<h1>Bah Alors, on est pas premium ?</h1>
+<br>
+<div class="grid-x grid-padding-x">
+  <div class="cell small-4 text-center"><h2 class="subheader">Abonnement a 100e / mois </h2><img src="https://afghanistan-parsa.org/wp-content/uploads/2013/03/paypaldonatenow.png"> .</div>
+  <div class="cell small-4 text-center"><h2 class="subheader">Abonnement a 150e / mois</h2> <img src="https://afghanistan-parsa.org/wp-content/uploads/2013/03/paypaldonatenow.png"> </div>
+  <div class="cell small-4 text-center"><h2 class="subheader">Abonnement a 46584e / mois</h2> <img src="https://afghanistan-parsa.org/wp-content/uploads/2013/03/paypaldonatenow.png"> </div>
+</div>
+
 <?php
 require_once 'view/footer.php';
 ?>
